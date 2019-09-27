182643fc2a435561ed506763566d2f9fe4fd117b
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2019-09-27 14:37:00 +0900

    fix: contact_mailer.rb修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,6 @@
 class ContactMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.contact_mail.subject
-  #
-  def contact_mail
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def contact_mail(contact)
+    @contact = contact  
+    mail to: 'info@tomarumaru.com', subject: "お問い合わせが来ました"
   end
 end
